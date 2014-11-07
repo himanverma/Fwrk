f96e78335cbc39be1b03b9807bb187c271be005f
ajay <ajayp944@gmail.com>
2014-11-07 18:53:31 +0530

    Added new files

---
+++ oride/Model/User.php
@@ -0,0 +1,13 @@
+<?php 
+App::uses('AppModel', 'Model');
+
+/**
+ * Application model for Cake.
+ *
+ * Add your application-wide methods in the class below, your models
+ * will inherit them.
+ *
+ * @package       app.Model
+ */
+class UserModel extends AppModel {
+}
